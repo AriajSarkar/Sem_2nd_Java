f35ec5be6f51e3b5c0517754e2509f3cf5a0d422
Ariaj Sarkar <rajsarkar02205@gmail.com>
2025-02-18 22:33:10 +0530

    Add implementation to print alphabets in reverse order in main.java

---
+++ Lab_Q/Q7_Rev_Alphabets/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Alphabets in Reverse Order:");
+
+        for (char ch = 'Z'; ch >= 'A'; ch--) {
+            System.out.print(ch + " ");
+        }
+
+        System.out.println(); // New line
+
+        for (char ch = 'z'; ch >= 'a'; ch--) {
+            System.out.print(ch + " ");
+        }
+    }
+}
