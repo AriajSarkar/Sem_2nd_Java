064fd0a0210946d637a39fdcfdd61112f8bed891
Ariaj Sarkar <rajsarkar02205@gmail.com>
2025-02-18 22:33:06 +0530

    Add implementation to find perfect numbers under 1000 in main.java

---
+++ Lab_Q/Q6_PerfectNumList/main.java
@@ -0,0 +1,26 @@
+public class  main {
+
+    public static void main(String[] args) {
+        System.out.println("Perfect numbers under 1000:");
+
+        for (int num = 1; num <= 1000; num++) {
+            if (isPerfect(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    // Method to check if a number is perfect
+    private static boolean isPerfect(int num) {
+        int sum = 0;
+
+        // Find divisors and add them
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == num;
+    }
+}
