b83abbad028d1abb7fc30dac568abe94f071bdb2
Ariaj Sarkar <rajsarkar02205@gmail.com>
2025-02-18 22:32:55 +0530

    Add GCD and LCM calculation implementation in main.java

---
+++ Lab_Q/Q4_GCD_LCM/main.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        // Compute GCD
+        int gcd = findGCD(num1, num2);
+
+        // Compute LCM using formula
+        int lcm = (num1 * num2) / gcd;
+
+        // Display results
+        System.out.println("GCD: " + gcd);
+        System.out.println("LCM: " + lcm);
+
+        scanner.close();
+    }
+
+    // Function to find GCD using Euclidean Algorithm
+    private static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
