7ff94d4122fc2475e820ff9b18d873fb072d9ed9
Ariaj Sarkar <rajsarkar02205@gmail.com>
2025-05-20 00:38:31 +0530

    Refactor palindrome check program for improved readability

--- 20_Q/Q6_PalindromeCheck.java
+++ 20_Q/Q6_PalindromeCheck.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Q6_PalindromeCheck {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String input = scanner.nextLine();k
-        
-        StringBuilder reversed = new StringBuilder(input).reverse();
-        String reversedStr = reversed.toString();
-        
+        String input = scanner.nextLine();
+
+        StringBuilder sb = new StringBuilder(input).reverse();
+        String reversedStr = sb.toString();
+
         if (input.equals(reversedStr)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not palindrome");
         }
-        
+
         scanner.close();
     }
 }
